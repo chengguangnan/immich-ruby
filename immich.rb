8a0a6a350137189f24d36d1c269d17471102b71e
cheng <cheng@m2>
2024-08-03 19:57:01 +0800

    more works

--- immich.rb
+++ immich.rb
@@ -1,46 +1,114 @@
 # frozen_string_literal: true
 
 require 'http'
+require 'debug'
 
-API_KEY = 'Sr0WJWYFECjCIRU7VUJIDakBkNh1U8FVv4fPvinQeA' # replace with a valid api key
-BASE_URL = 'http://mac-m1.local:2283' # replace as needed
+require 'yaml'
+
+class Immich
+  def initialize
+    config = YAML.load_file("#{Dir.home}/.config/immich/auth.yml")
+    @key = config['apiKey']
+    @host = config['instanceUrl']
+  end
+
+  def download_info(album_id)
+    url = "#{@host}/api/download/info"
+    http_post.post(url, json: { "albumId": album_id }).parse
+  end
+
+  def download_archive(asset_id, dir)
+    # /api/download/archive has the Live Photo audio file
+
+    puts 'download_asset_zip'
+    url = "#{@host}/api/download/archive"
+
+    resp = HTTP.headers(accept: 'application/octet-stream', 'x-api-key': @key).post(
+      url, json: { "assetIds": [asset_id] }
+    )
+    out = File.join(dir, "#{asset_id}.zip")
+    File.open(out, 'w') { |file| file.write(resp.body) }
+  end
+
+  # def download_asset(asset_id, dir)
+  #   url = "#{@host}/api/assets/#{asset_id}/original"
+  #
+  #   resp = HTTP.headers(accept: 'application/octet-stream', 'x-api-key': @key).get(
+  #     url
+  #   )
+  #   out = File.join(dir, "#{asset_id}.zip")
+  #   File.open(out, 'w') { |file| file.write(resp.body) }
+  # end
+
+  def get_album_info(album_id)
+    http_get "#{@host}/api/albums/#{album_id}"
+  end
+
+  def albums
+    http_get "#{@host}/api/albums"
+  end
+
+  def http_get(url)
+    HTTP.headers(accept: 'application/json', 'x-api-key': @key).get(url).parse
+  end
+
+  def http_post
+    HTTP.headers(:accept => 'application/json', 'x-api-key' => @key)
+  end
+end
 
 require 'optimist'
 
 opts = Optimist.options do
-  opt :albumn_id, 'Address of Google Refine'
+  opt :album, 'Album Name', type: :string
 end
 
-Optimist.die :albumn_id, ':albumn_id' if opts[:albumn_id]
+Optimist.die :album, '-album required' unless opts[:album_given]
 
 puts opts
 
-def get_assets(album_id)
-  payload = {
-    "albumId": album_id
-  }
-  url = "#{BASE_URL}/api/download/info"
-  resp = HTTP.headers(:accept => 'application/json', 'x-api-key' => API_KEY).post(url, json: payload).body
-  JSON.parse(resp)
-end
+output_dir = 'downloads'
 
-def download_asset(asset_id)
-  url = '/api/download/archive'
+immich = Immich.new
 
-  resp = HTTP.headers('Content-Type': 'application/json', 'Accept': 'application/octet-stream', 'x-api-key': API_KEY).post(
-    BASE_URL + url, json: { "assetIds": [asset_id] }
-  )
+require 'fileutils'
 
-  File.open("output/#{asset_id}.zip", 'w') { |file| file.write(resp.body) }
-end
+immich.albums.each do |album|
+  next unless album['albumName'] == opts[:album]
 
-data = get_assets('56f834dc-e8fc-4a13-ba10-1e60256dc011')
+  download_info = immich.download_info(album['id'])
 
-# 1965795418
-puts data['totalSize'] / 1024 / 1024
+  album_info = immich.get_album_info(album['id'])
 
-data['archives'].each do |archive|
-  archive['assetIds'].each do |asset|
-    download_asset(asset)
+  File.open(File.join(output_dir, "#{album['albumName']}.album-info.yaml"), 'w') do |out|
+    YAML.dump(album_info, out)
+  end
+
+  File.open(File.join(output_dir, "#{album['albumName']}.album-archive.yaml"), 'w') do |out|
+    YAML.dump(download_info, out)
+  end
+
+  #  debugger
+
+  dir = "#{output_dir}/#{album['albumName']}"
+
+  FileUtils.mkdir_p(dir)
+
+  if download_info['statusCode'].nil?
+
+    puts format('%d MB', (download_info['totalSize'] / 1024 / 1024))
+
+    i = 0
+    download_info['archives'].each do |archive|
+      archive['assetIds'].each do |asset|
+        i += 1
+        puts format('%d / %d', i, album_info['assetCount'])
+        immich.download_archive(asset, dir)
+      end
+    end
+
+  else
+    # {"statusCode"=>500, "message"=>"Internal server error"}
+    puts download_info
   end
 end
